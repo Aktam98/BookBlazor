91970621cee54a1325e45345f3bc0f6bb4894abd
lenovo <lenovo@DESKTOP-QKNFAL1>
2021-02-15 10:21:15 +0300

    add services

--- Book/Book/Startup.cs
+++ Book/Book/Startup.cs
@@ -29,6 +29,7 @@ namespace Book
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<BookServices>()
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
